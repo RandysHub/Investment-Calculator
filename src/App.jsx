acf12d2942ae4fd4adcd7f8135197e12dc9e4c05
RandysHub <106208354+RandysHub@users.noreply.github.com>
2024-03-29 01:27:21 -0400

    fixed bug where duration number below 0 crashed the app

--- src/App.jsx
+++ src/App.jsx
@@ -21,11 +21,15 @@ function App() {
     });
   }
 
+
+
+  const inputIsValid = userInput.duration >= 1;
   return (
     <>
       <Header />
       <UserInput userInput={userInput} onChangeInput={handleChange} />
-      <Results input={userInput} />
+      {!inputIsValid && <p className="center">Please enter duration greater than 0</p>}
+      {inputIsValid && <Results input={userInput} />}
     </>
   )
 }
